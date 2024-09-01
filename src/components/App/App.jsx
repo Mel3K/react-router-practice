44647021cfe36f4d7b36abf739e3d32ac9977192
Meleah Roe <melroe19@gmail.com>
2024-09-01 13:36:31 -0500

    entering routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,26 @@
 import './App.css';
 import StudentForm from '../StudentForm/StudentForm.jsx';
 import StudentList from '../StudentList/StudentList.jsx';
+import { HashRouter as Router, Route } from "react=router";
+
 
 
 function App() {
     return (
+        <Router>
         <div className="App">
             <header>
                 <h1>GitHub Student List</h1>
                 {/* Nice spot for a nav, eh? */}
             </header>
-            <StudentForm />
+            <Route path="studentForm/">
+    <StudentForm />
+           <Route path="studentList/">
             <StudentList />
+
         </div>
+        </Router>
+        <Route/>
     );
 }
 
